e45359c2119c30347b4f833e160b133f6e77e2e6
Sushanth Kummitha <sushanth.kummitha@needl.ai>
2024-02-22 15:07:59 +0530

    component and props

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,22 @@
 <script>
+  import Greet from "./components/Greet.svelte";
+
+  const name = "Modi";
+  const superHeroName = "Prime Minister of India";
+
+  const person = {
+    name: "Virat Kohli",
+    superHeroName: "GOAT",
+  };
 </script>
 
-<main></main>
+<main>
+  <Greet superHeroName="HanuMan" />
+  <Greet name="Phaneendra" superHeroName="IronMan" />
+  <Greet name="Krishna" />
+  <Greet {name} {superHeroName} />
+  <Greet {...person} />
+</main>
 
 <style>
 </style>
---
+++ src/components/Greet.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let name = "Sushanth";
+  export let superHeroName = "Superman";
+</script>
+
+<p>
+  Hello <b>
+    {name}
+  </b>
+  a.k.a
+  <b>
+    {superHeroName}
+  </b>
+</p>
