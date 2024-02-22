d0ef6841831d5fe17486c8256e2fdf457fce0eeb
Sushanth Kummitha <sushanth.kummitha@needl.ai>
2024-02-22 21:58:13 +0530

    slot props

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,7 @@
   import Outer from "./components/Outer.svelte";
   import Card from "./components/Card.svelte";
   import Button from "./components/Button.svelte";
+  import Names from "./components/Names.svelte";
 
   const name = "Modi";
   const superHeroName = "Prime Minister of India";
@@ -34,17 +35,17 @@
 </script>
 
 <main>
-  <Greet superHeroName="HanuMan" />
+  <!-- <Greet superHeroName="HanuMan" />
   <Greet name="Phaneendra" superHeroName="IronMan" />
   <Greet name="Krishna" />
   <Greet {name} {superHeroName} />
-  <Greet {...person} />
-  <button on:click={handlePopupOpen}>Open Popup</button>
+  <Greet {...person} /> -->
+  <!-- <button on:click={handlePopupOpen}>Open Popup</button>
   {#if showPopup}
     <Popup onClose={handlePopupClose} on:close={handlePopupClose} />
-  {/if}
-  <br />
-  <Outer on:greet={greetHandler} />
+  {/if} -->
+  <!-- <br /> -->
+  <!-- <Outer on:greet={greetHandler} /> -->
   <!-- <Card />
   <Card>Card Content</Card>
   <Card>
@@ -53,7 +54,7 @@
   <Card>
     <img src="https://picsum.photos/300" alt />
   </Card> -->
-  <Card>
+  <!-- <Card>
     <div slot="header">
       <h2>Header</h2>
     </div>
@@ -64,7 +65,16 @@
       <Button>View More</Button>
     </div>
   </Card>
-  <Card />
+  <Card /> -->
+  <Names>
+    <h4 slot="hero" let:firstName let:lastName>{firstName} {lastName}</h4>
+  </Names>
+  <Names>
+    <h4 slot="hero" let:firstName let:lastName>{lastName} {firstName}</h4>
+  </Names>
+  <Names>
+    <h4 slot="hero" let:firstName>{firstName}</h4>
+  </Names>
 </main>
 
 <style>
---
+++ src/components/Names.svelte
@@ -0,0 +1,22 @@
+<script>
+  const names = [
+    {
+      firstName: "Virat",
+      lastName: "Kohli",
+    },
+    {
+      firstName: "Sachin",
+      lastName: "Tendulkar",
+    },
+    {
+      firstName: "MS",
+      lastName: "Dhoni",
+    },
+  ];
+</script>
+
+<h2>Names</h2>
+{#each names as name (name)}
+  <!-- <p>{name.firstName} {name.lastName}</p> -->
+  <slot name="hero" firstName={name.firstName} lastName={name.lastName} />
+{/each}
